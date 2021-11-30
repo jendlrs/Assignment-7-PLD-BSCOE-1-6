fd3706c68f62173d3bd6e8b0154cee8aa8737407
jendlrs <jensennics.dlrs@gmail.com>
2021-11-30 19:45:21 +0800

    Get user input

--- word_vowel_consonant.py
+++ word_vowel_consonant.py
@@ -9,6 +9,11 @@
 #consonants: 8
 
 #Step 1: Ask user to input a sentence
+def getSentence ():
+    user_input_ = input ("Enter your sentence: ")
+    return user_input_
 #Step 2: Count number of words, store, display
 #Step 2: Count number of vowels and consonants, display
     #list vowels and consonants
+
+user_input = getSentence()
