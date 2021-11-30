bd2d38e0951f3a1c29e0c4f46fb1456169a2184c
jendlrs <jensennics.dlrs@gmail.com>
2021-11-30 19:42:52 +0800

    Listed information and partial steps for the program.

---
+++ word_vowel_consonant.py
@@ -0,0 +1,14 @@
+#Program 1:
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
+#Step 1: Ask user to input a sentence
+#Step 2: Count number of words, store, display
+#Step 2: Count number of vowels and consonants, display
+    #list vowels and consonants
