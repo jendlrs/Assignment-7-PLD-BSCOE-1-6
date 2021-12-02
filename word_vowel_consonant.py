d1238ad64a984fd036199a43ffdb0a519bcda681
jendlrs <jensennics.dlrs@gmail.com>
2021-12-02 16:37:02 +0800

    I added greetings and line breaks to make it look neat.

--- word_vowel_consonant.py
+++ word_vowel_consonant.py
@@ -7,15 +7,17 @@
 #words: 3
 #vowels: 6
 #consonants: 8
-
+name= input("\nWelcome! What is your name? ")
+print (f"\nHi {name}! This program will look into your sentence. It will help you count words, vowels, and consonants.\n")
 #Step 1: Ask user to input a sentence
 def getSentence ():
-    user_input_ = input ("Enter your sentence: ")
+    user_input_ = input ("To continue, please Enter your sentence below.\n-->")
 #Step 2: Count number of words, store
     words = user_input_.split()
     number_words = len (words)
     #Step 3: Count number of vowels and consonants
-    #add global variable for consonant and vowel
+
+    #Initial number for vowels and consonants
     vowels = 0
     consonants =0
     for i in user_input_:
@@ -27,9 +29,10 @@ def getSentence ():
             if i == " ":
                 consonants= consonants -1
 #Display
-    print (f"The number of words in the sentence is {number_words}")
+    
+    print (f"\nThe number of words in the sentence is {number_words}")
     print (f"The number of vowels in the sentence is {vowels}")
-    print (f"The number of consonants in the sentence is {consonants} ")
+    print (f"The number of consonants in the sentence is {consonants} \n")
 
 getSentence()
 
